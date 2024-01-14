c41edce236d726d36753dcfd9cf75a34cc33bb55
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-14 17:06:17 +0530

    Merge changes from remote

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+
+import mensheroimg from "./images/baner-right-image-02.jpg"
+import womensheroimg from "./images/baner-right-image-01.jpg"
+import kidsheroimg from "./images/baner-right-image-03.jpg"
+
+
+export const menproducts = [
+    { id: 1, name: 'Classic Spring', price: 120, image: mensheroimg },
+    { id: 2, name: ' Spring', price: 120, image: mensheroimg },
+    { id: 3, name: 'Classic ', price: 120, image: mensheroimg },
+   
+    // Add more products as needed
+  ];
+export const womenProducts = [
+    { id: 1, name: 'Classic Spring', price: 120, image: womensheroimg },
+    { id: 2, name: ' Spring', price: 120, image: womensheroimg },
+    { id: 3, name: 'Classic ', price: 120, image: womensheroimg },
+
+    // Add more products as needed
+  ];
+export const kidsProducts = [
+    { id: 1, name: 'Classic Spring', price: 120, image: kidsheroimg },
+    { id: 2, name: ' Spring', price: 120, image: kidsheroimg },
+    { id: 3, name: 'Classic ', price: 120, image: kidsheroimg},
+ 
+    // Add more products as needed
+  ];
---
+++ src/componets/LatestProductWrapper.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import StarRatigSvg from './StarRatigSvg';
+
+
+const LatestProductWrapper = ({sectionName,data}) => {
+  return (
+    <div id="mens" className="flex flex-col gap-10 w-full px-6 ">
+      <div>
+          <h1 className="text-[32px] font-bold">{sectionName} Latest</h1>
+          <p className="text-sm text-gray-400 italic">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            asperiores nemo inventore, amet eveniet repudiandae est cumque.
+          </p>
+        </div>
+        
+      <div className="flex  gap-8 relative ">
+        <div className="border -left-6 z-10 top-32 absolute p-3 border-black">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                    >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                    />
+                    </svg>
+                </div>
+            <div className='overflow-x-scroll flex  gap-8'>
+                    {data.map((product) => (
+                <div key={product.id} className="flex flex-col gap-4 ">
+                    <div className='h-80 w-140'>
+                        <img className='object-cover h-full w-full' src={product.image} alt={product.name} />
+                    </div>
+                    <div className="flex  justify-between">
+                    <div className="flex flex-col gap-2">
+                        <h4 className="text-[23px] font-bold">{product.name}</h4>
+                        <h5 className="text-gray-500">${product.price}</h5>
+                    </div>
+                    <div className="flex gap-1">
+                        <StarRatigSvg/>
+                        <StarRatigSvg/>
+                        <StarRatigSvg/>
+                        <StarRatigSvg/>
+                        <StarRatigSvg/>
+                    </div>
+                    </div>
+          </div>
+        ))}
+            </div>
+            <div className="border -right-6 top-32 absolute p-3  border-black">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+        />
+      </svg>
+    </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default LatestProductWrapper;
