265638f64610168cd61204e9ca81b1c3d2649169
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-15 18:14:44 +0530

    Merge changes from remote

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -6,20 +6,20 @@ const Footer = () => {
       <div className="flex  px-16 w-full ">
         <div className="md:flex gap-10 py:6 md:py-8 lg:py-10 border-b-2 border-gray-300  w-full">
           <div className="">shantanuchavhan002@gmail.com</div>
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 sm:py-4">
             <h1 className="pb-1 font-bold">Shopping Categories</h1>
             <h1>Men,s Shopping </h1>
             <h1>women,s Shopping</h1>
             <h1>kid's Shopping</h1>
           </div>
 
-          <div className="flex flex-col gap-3 sm:pb-3 ">
+          <div className="flex flex-col gap-3 sm:py-4 ">
             <h1 className="pb-1 font-bold">Homepage</h1>
             <h1>About Us </h1>
             <h1>Help</h1>
             <h1>Contact Us</h1>
           </div>
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col sm:py-4 gap-3">
             <h1 className="pb-1 font-bold">Shopping Categories</h1>
             <h1>Help </h1>
             <h1>FAQ's</h1>
