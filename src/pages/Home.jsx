77855cfba5daa412096d39d9ceb0ab2cea2b4b31
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-17 18:20:26 +0530

    Merge changes from remote

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className="flex flex-col gap-2  p-4 ">
       <div id="home" className="lg:flex gap-8   ">
-        <div className="flex-1  lg:w-50 md:pb-10  lg:pb-0  md:h-[300px]  lg:h-full relative">
+        <div className="flex-1  lg:w-50 pb-10  lg:pb-0  h-[300px]  lg:h-full relative">
           <img className='object-cover w-full  h-full' src={leftimage} alt="" />
           <div className="absolute top-0 left-0 flex text-white items-center justify-center w-full h-full">
             <h1>Hire me</h1>
@@ -18,28 +18,28 @@ const Home = () => {
         </div>
 
         <div className="w-50 flex-1 w-full md:grid md:grid-cols-2 gap-8">
-          <div className="relative md:h-[200px] lg:h-full">
+          <div className="relative h-[200px] lg:h-full">
             <img className='w-full h-full object-cover' src={womensheroimg} alt="" />
             <div className="text-white absolute  top-0 flex-col gap-3 left-0 flex items-center justify-center w-full h-full">
               <h1 className="text-[28px] font-extrabold ">Women</h1>
               <h2 className="italic">Best Cloth for Women</h2>
             </div>
           </div>
-          <div className="relative md:h-[200px] lg:h-full">
+          <div className="relative h-[200px] lg:h-full">
             <img className='w-full h-full object-cover'  src={mensheroimg} alt="" />
             <div className="absolute top-0 left-0 flex flex-col gap-3 text-white items-center justify-center w-full h-full">
               <h1 className="text-[28px] font-extrabold ">Men</h1>
               <h2 className="italic">Best Cloth for Men</h2>
             </div>
           </div>
-          <div className="relative md:h-[200px] lg:h-full">
+          <div className="relative h-[200px] lg:h-full">
             <img className='w-full h-full object-cover'  src={kidsheroimg} alt="" />
             <div className="absolute top-0 left-0 flex flex-col gap-3 text-white items-center justify-center w-full h-full">
               <h1 className="text-[28px] font-extrabold ">Kids</h1>
               <h2 className="italic">Best Cloth for Kids</h2>
             </div>
           </div>
-          <div className="relative md:h-[200px] lg:h-full">
+          <div className="relative h-[200px] lg:h-full">
             <img className='w-full h-full object-cover'  src={Accesoriesheroimg} alt="" />
             <div className="absolute top-0 left-0 flex flex-col gap-3 text-white items-center justify-center w-full h-full">
               <h1 className="text-[28px] font-extrabold ">Accresories</h1>
