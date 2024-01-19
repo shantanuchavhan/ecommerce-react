ad3f3de4472c496c4269964e7746b7074e698287
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-19 17:41:26 +0530

    Merge changes from remote

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -138,7 +138,7 @@ const Checkout = () => {
             </div>
           </ul>
 
-          <div className="w-full md:w-[40%]   pt-3  md:pt-0 border-t  md:border-l flex flex-col  min-h-[50vh] justify-between  pl-4">
+          <div className={`w-full md:w-[40%]   pt-3   md:pt-0 ${window.innerWidth >= 768 ? '' : 'border-t'}  md:border-l flex flex-col  min-h-[50vh] justify-between  pl-4`}>
             <div>
               <div className="flex flex-col gap-2">
                 <h1>Price Details(1item)</h1>
