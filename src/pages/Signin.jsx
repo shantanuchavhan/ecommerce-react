8762154ba164377b188c0e97318fdcd12c819edc
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-19 13:18:46 +0530

    Merge changes from remote

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,98 +1,44 @@
-import React from 'react'
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBIcon,
-} from 'mdb-react-ui-kit'
+import React, { useState } from 'react'
 
-function Signin() {
+const Signin = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleRegister = () => {
+    // Implement registration logic here
+    console.log('Registering with:', email, password)
+  }
+
   return (
-    <MDBContainer fluid>
-      <MDBRow className="d-flex justify-content-center align-items-center h-100">
-        <MDBCol col="12">
-          <MDBCard
-            className="bg-dark text-white my-5 mx-auto"
-            style={{ borderRadius: '1rem', maxWidth: '400px' }}
-          >
-            <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
-              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-              <p className="text-white-50 mb-5">
-                Please enter your login and password!
-              </p>
-
-              <MDBInput
-                wrapperClass="mb-4 mx-5 w-100"
-                labelClass="text-white"
-                label="Email address"
-                id="formControlLg"
-                type="email"
-                size="lg"
-              />
-              <MDBInput
-                wrapperClass="mb-4 mx-5 w-100"
-                labelClass="text-white"
-                label="Password"
-                id="formControlLg"
-                type="password"
-                size="lg"
-              />
-
-              <p className="small mb-3 pb-lg-2">
-                <a class="text-white-50" href="#!">
-                  Forgot password?
-                </a>
-              </p>
-              <MDBBtn outline className="mx-2 px-5" color="white" size="lg">
-                Login
-              </MDBBtn>
-
-              <div className="d-flex flex-row mt-3 mb-5">
-                <MDBBtn
-                  tag="a"
-                  color="none"
-                  className="m-3"
-                  style={{ color: 'white' }}
-                >
-                  <MDBIcon fab icon="facebook-f" size="lg" />
-                </MDBBtn>
-
-                <MDBBtn
-                  tag="a"
-                  color="none"
-                  className="m-3"
-                  style={{ color: 'white' }}
-                >
-                  <MDBIcon fab icon="twitter" size="lg" />
-                </MDBBtn>
-
-                <MDBBtn
-                  tag="a"
-                  color="none"
-                  className="m-3"
-                  style={{ color: 'white' }}
-                >
-                  <MDBIcon fab icon="google" size="lg" />
-                </MDBBtn>
-              </div>
-
-              <div>
-                <p className="mb-0">
-                  Don't have an account?{' '}
-                  <a href="#!" class="text-white-50 fw-bold">
-                    Sign Up
-                  </a>
-                </p>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <div className="flex items-center justify-center min-h-[70vh] w-[100vw]">
+      <div className="max-w-md mx-auto w-full md:w-[70%] lg:w-[50%]   p-6  bg-black text-white border border-black md:rounded  shadow-md">
+        <h2 className="text-2xl font-semibold mb-6">Login</h2>
+        <label className="block mb-4">
+          Email:
+          <input
+            className="border border-gray-600 rounded w-full py-2 px-3 bg-gray-900 text-white"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className="block mb-4">
+          Password:
+          <input
+            className="border border-gray-600 rounded w-full py-2 px-3 bg-gray-900 text-white"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button
+          className="bg-white text-black py-2 px-4 rounded  hover:bg-gray-1000 focus:outline-none focus:shadow-outline-gray"
+          onClick={handleRegister}
+        >
+          Login
+        </button>
+      </div>
+    </div>
   )
 }
 
